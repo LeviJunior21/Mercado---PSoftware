6bacff43a9dc1b411bbade17738e0586f87b7a06
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2023-03-20 09:55:21 -0300

    Primeiro commit

---
+++ src/Produto.java
@@ -0,0 +1,30 @@
+import java.util.Objects;
+
+public class Produto {
+	private String nome;
+	private String fabricante;
+	private double preco;
+	
+	public Produto(String nome, String fabricante, double preco) {
+		this.nome = nome;
+		this.fabricante = fabricante;
+		this.preco = preco;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(fabricante, nome);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(fabricante, other.fabricante) && Objects.equals(nome, other.nome);
+	}
+}
